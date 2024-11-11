53cfe77966ef4b480665f482177671e1dc8f75cc
Purukan-Jeremy <s22210214@student.unklab.ac.id>
2024-11-12 07:23:11 +0800

    add images

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { MouseTrail } from "@stichiboi/react-elegant-mouse-trail";
+import GymPhoto from '../../assets/img/strong-man-training-gym.jpg';
+import GymPhoto2 from '../../assets/img/people-doing-indoor-cycling.jpg';
+import GymPhoto3 from '../../assets/img/close-up-kettlebells-workout-gym.jpg';
 
 const Home = () => {
   return (
     <div>
-    <MouseTrail strokeColor="rgb(240, 219, 79)" lineWidthStart={10} />
+      <MouseTrail strokeColor="rgb(240, 219, 79)" lineWidthStart={10} />
       <section className="home-section">
         <div className="home-content">
           <h1 className="home-title">Welcome to GymRat</h1>
@@ -13,6 +16,11 @@ const Home = () => {
             workout brings you a step closer to greatness. Stay strong, stay
             motivated, and keep pushing forward!"
           </p>
+          <div className="image-container">
+            <img src={GymPhoto2} alt="gym" className="gym-image side-image" />
+            <img src={GymPhoto} alt="gym" className="gym-image center-image" />
+            <img src={GymPhoto3} alt="gym" className="gym-image side-image" />
+          </div>
         </div>
       </section>
     </div>
