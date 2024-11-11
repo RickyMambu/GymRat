2780305fe943ec550d247dfab73d611e3db19f21
Purukan-Jeremy <s22210214@student.unklab.ac.id>
2024-11-12 06:47:01 +0800

    add home and mouse trail

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../src/assets/css/index.css';
 import Navbar from './components/NavBar'
+import Home from './components/Home';
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Home/>
       <section>
       </section>
     </div>
---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { MouseTrail } from "@stichiboi/react-elegant-mouse-trail";
+
+const Home = () => {
+  return (
+    <div>
+      <MouseTrail 
+        strokeColor="rgb(240, 219, 79)"
+        lineWidthStart={10}
+      />
+    </div>
+  )
+}
+
+export default Home
