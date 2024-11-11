868f022b5af546008dc8e88aa0eca6f7efaa4e80
Purukan-Jeremy <s22210214@student.unklab.ac.id>
2024-11-12 06:56:42 +0800

    smo kelas comgraph kse trus sbntr jh

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React from "react";
 import { MouseTrail } from "@stichiboi/react-elegant-mouse-trail";
 
 const Home = () => {
   return (
     <div>
-      <MouseTrail 
-        strokeColor="rgb(240, 219, 79)"
-        lineWidthStart={10}
-      />
+    <MouseTrail strokeColor="rgb(240, 219, 79)" lineWidthStart={10} />
+      <section className="home-section">
+        <div className="home-content">
+          <h1 className="home-title">Welcome to GymRat</h1>
+          <p className="home-paragraph">
+            "Push yourself because no one else is going to do it for you. Every
+            workout brings you a step closer to greatness. Stay strong, stay
+            motivated, and keep pushing forward!"
+          </p>
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
