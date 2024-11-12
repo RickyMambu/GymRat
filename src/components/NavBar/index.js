7571722401b555a83fb9cc2cdd4d8386f9e1ad3c
Purukan-Jeremy <s22210214@student.unklab.ac.id>
2024-11-12 21:16:01 +0800

    landing page done

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { FaAppleAlt, FaDumbbell, FaChartLine, FaInfoCircle } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1 className="navbar-logo">GymRat</h1>
       <ul className="navbar-links">
-        <li><a href="#diet">Diet</a></li>
-        <li><a href="#exercise">Exercise</a></li>
-        <li><a href="#progress">Progress</a></li>
-        <li><a href="#information">Information</a></li>
+        <li><a href="#diet"><FaAppleAlt className="icon" /> Diet</a></li>
+        <li><a href="#exercise"><FaDumbbell className="icon" /> Exercise</a></li>
+        <li><a href="#progress"><FaChartLine className="icon" /> Progress</a></li>
+        <li><a href="#information"><FaInfoCircle className="icon" /> Information</a></li>
       </ul>
     </nav>
   );
