8c7210a824e38fe3c7446419961199c7abff2980
Emily <s22210189@student.unklsb.ac.id>
2024-11-14 08:44:24 -0800

    add contact us

---
+++ src/components/Contact/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Contact = () => (
+  <section id="contact" className="contact full">
+    <div className="container">
+      <div className="sec-top">
+        <h1>
+          Contact <span>Us</span>
+        </h1>
+      </div>
+      <div className="contact-boxes">
+        <div className="contact-box">
+          <i className="fa-solid fa-phone" />
+          <h3>Phone</h3>
+          <p>082193492409</p>
+        </div>
+        <div className="contact-box">
+          <i className="fa-solid fa-envelope" />
+          <h3>Email</h3>
+          <p>info@example.com</p>
+        </div>
+        <div className="contact-box">
+          <i className="fa-solid fa-map-marker-alt" />
+          <h3>Address</h3>
+          <p>Klabat university </p>
+        </div>
+      </div>
+      <form className="contact-form">
+        <input type="text" placeholder="Your Name" required />
+        <input type="email" placeholder="Your Email" required />
+        <textarea placeholder="Your Message" required></textarea>
+        <button type="submit" className="btn">
+          Send Message
+        </button>
+      </form>
+    </div>
+  </section>
+);
+
+export default Contact;
