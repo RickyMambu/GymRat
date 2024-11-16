1d9f6fdfdcc806f0ce2d3c5a1bd88c6dd405e93d
Aruperes <revandoaruperes6@gmail.com>
2024-11-16 17:39:17 +0800

    fix signIn

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import '../../App.css';
+import React from "react";
+import "../../App.css";
 
-const SignUp = () => {
-    
+const SignIn = () => {
   return (
-    <div className="signup-container">
-      <h2 className="signup-title">Join GymRat</h2>
-      <form className="signup-form">
-        <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" placeholder="Enter your username" required />
-        
-        <label htmlFor="email">Email Address</label>
-        <input type="email" id="email" name="email" placeholder="Enter your email" required />
-        
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" placeholder="Enter your password" required />
-        
-        <button type="submit" className="signup-button">Sign Up</button>
-      </form>
+    <div className="signup-wrapper">
+      <div className="welcome-section">
+        <h1>Welcome to GymRat!</h1>
+        <p>
+          No matter what your fitness goal may be, you'll find everything you
+          need at GymRat.
+        </p>
+        <button className="reg-btn">Register</button>
+      </div>
+      <div className="signup-container">
+        <h2 className="signup-title">Sign In</h2>
+        <form className="signup-form">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            placeholder="Enter  your username"
+            required
+          />
+
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Enter your password"
+            required
+          />
+
+          <button type="submit" className="signup-button">
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
 
-export default SignUp;
+export default SignIn;
