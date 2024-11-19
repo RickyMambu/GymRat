2d2af0f5920ff8d9ee0e02c405e9894503c9d3b8
RickyMambu <rickymambu14@gmail.com>
2024-11-19 23:19:41 +0800

    picture responsif

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -20,109 +20,108 @@ const Slider = () => {
   }, []);
 
   return (
+    <section className="about-section">
 
-    <div className="container">
-      <section id="experience">
-        {/* About Us */}
-        <div className="about">
-          <div className="about_main">
-            <div className="about_text">
-              <h1>
-                <span>About</span>Us
-              </h1>
+      <div className="container">
+          {/* About Us */}
+          <div className="about">
+            <div className="about_main">
+              <div className="about_text">
+                <h1>
+                  <span>About</span>Us
+                </h1>
+              </div>
             </div>
+          </div>
 
+        {/* Slider Section */}
+        <div className="slider-wrapper">
+          <div ref={sliderRef} className="slider">
+            <div className="slide">
+              <img src="./images/about1.jpg" alt="Image 1" />
+              <div className="slide-text">
+                GymRat is your trusted companion in achieving fitness and health
+                goals through expert guidance and tools
+              </div>
+            </div>
+            <div className="slide">
+              <img src="./images/about2.jpg" alt="Image 2" />
+              <div className="slide-text">
+                We are dedicated to providing top-tier fitness programs tailored
+                to every individual’s journey
+              </div>
+            </div>
+            <div className="slide">
+              <img src="./images/about3.jpg" alt="Image 3" />
+              <div className="slide-text">
+                Our diet plans are crafted to complement your fitness regime,
+                ensuring optimal health and performance
+              </div>
+            </div>
+          </div>
+
+          <div className="slider-nav">
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
           </div>
         </div>
-      </section>
 
-      {/* Slider Section */}
-      <div className="slider-wrapper">
-        <div ref={sliderRef} className="slider">
-          <div className="slide">
-            <img src="./images/about1.jpg" alt="Image 1" />
-            <div className="slide-text">
-              GymRat is your trusted companion in achieving fitness and health
-              goals through expert guidance and tools
+        {/* Team Section */}
+        <div className="team">
+          <h1>
+            Our<span> Team</span>
+          </h1>
+          <div className="team_box">
+            <div className="profile">
+              <img src="./images/ando.jpg" alt="ando-pic" className="about-pic" />
+              <div className="info">
+                <h2 className="name">Revando <br/>Aruperes</h2>
+              </div>
             </div>
-          </div>
-          <div className="slide">
-            <img src="./images/about2.jpg" alt="Image 2" />
-            <div className="slide-text">
-              We are dedicated to providing top-tier fitness programs tailored
-              to every individual’s journey
-            </div>
-          </div>
-          <div className="slide">
-            <img src="./images/about3.jpg" alt="Image 3" />
-            <div className="slide-text">
-              Our diet plans are crafted to complement your fitness regime,
-              ensuring optimal health and performance
-            </div>
-          </div>
-        </div>
-
-        <div className="slider-nav">
-          <a href="#"></a>
-          <a href="#"></a>
-          <a href="#"></a>
-        </div>
-      </div>
-
-      {/* Team Section */}
-      <div className="team">
-        <h1>
-          Our<span> Team</span>
-        </h1>
-        <div className="team_box">
-          <div className="profile">
-            <img src="./images/ando.jpg" alt="ando-pic" className="about-pic" />
-            <div className="info">
-              <h2 className="name">Revando Aruperes</h2>
+            <div className="profile">
+              <img
+                src="./images/ricky.jpg"
+                alt="ricky-pic"
+                className="about-pic"
+              />
+              <div className="info">
+                <h2 className="name">Ricky <br/>Mambu</h2>
+              </div>
 
             </div>
-          </div>
-          <div className="profile">
-            <img
-              src="./images/ricky.jpg"
-              alt="ricky-pic"
-              className="about-pic"
-            />
-            <div className="info">
-              <h2 className="name">Ricky Mambu</h2>
-            </div>
+            <div className="profile">
+              <img
+                src="./images/jeremy.png"
+                alt="jeremy-pic"
+                className="about-pic"
+              />
+              <div className="info">
+                <h2 className="name">Jeremy <br/>Purukan</h2>
 
-          </div>
-          <div className="profile">
-            <img
-              src="./images/jeremy.png"
-              alt="jeremy-pic"
-              className="about-pic"
-            />
-            <div className="info">
-              <h2 className="name">Jeremy Purukan</h2>
-
+              </div>
             </div>
-          </div>
-          <div className="profile">
-            <img
-              src="./images/mili.jpg"
-              alt="emily-pic"
-              className="about-pic"
-            />
-            <div className="info">
-              <h2 className="name">Emily Pangemanan</h2>
+            <div className="profile">
+              <img
+                src="./images/mili.jpg"
+                alt="emily-pic"
+                className="about-pic"
+              />
+              <div className="info">
+                <h2 className="name">Emily <br/>  Pangemanan</h2>
+              </div>
             </div>
-          </div>
-          <div className="profile">
-            <img src="./images/lika.jpg" alt="lika-pic" className="about-pic" />
-            <div className="info">
-              <h2 className="name">Noselycha Soriton</h2>
+            <div className="profile">
+              <img src="./images/lika.jpg" alt="lika-pic" className="about-pic" />
+              <div className="info">
+                <h2 className="name">Noselycha <br/> Soriton</h2>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
+
   );
 };
 
